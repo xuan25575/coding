fa034518c7b692133195698634366a57deef93e2
xuan25575 <1514244102@qq.com>
2018-11-06 15:58:40 +0800

    commit by 2018/11/06

---
+++ src/ccu/common/algorithms/sort/BubbleSort.java
@@ -0,0 +1,131 @@
+package ccu.common.algorithms.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    /**
+     * 冒泡排序   从第一个数开始，每次循环把最大的数放到最后。
+     * 冒泡排序的基本思想就是：从无序序列头部开始，进行两两比较，根据大小交换位置，
+     * 直到最后将最大（小）的数据元素交换到了无序队列的队尾，从而成为有序序列的一部分；
+     * 下一次继续这个过程，直到所有数据元素都排好序。
+     *
+     *  算法的核心在于每次通过两两比较交换位置，选出剩余无序序列里最大（小）的数据元素放到队尾。
+     *  时间复杂度  最好是O（n）, 最坏是（O(n^2)）
+     * @param arr
+     */
+    public static void bubbleSort(int arr []){
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for(int end = arr.length-1; end > 0;end--){  //减少比较次数
+            for(int i=0; i<end;i++){
+                if(arr[i] > arr[i+1] ){
+                    swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+
+    private  static void swap(int arr[] ,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    //比数器的实现。
+
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+
+    /**
+     * 生成随机数组
+     * @param maxSize  数组长度的最大值
+     * @param maxValue  随机的值的最大值
+     * @return int[]
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+//        int arr[]  = {2,5,3,7,15,4,10};
+//        bubbleSort(arr);
+//        printArray(arr);
+
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
---
+++ src/ccu/common/algorithms/sort/SelectionSort.java
@@ -0,0 +1,119 @@
+package ccu.common.algorithms.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    /**
+     * 选择排序
+     * 它的工作原理如下。首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
+     * 然后，再从剩余未排序元素中继续寻找最小（大）元素，
+     * 然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+     * @param arr
+     */
+    public static void selectionSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+        for(int i = 0; i < arr.length-1;i++){
+            int minIndex  = i;
+            for (int j = i+1; j < arr.length; j++) {
+                // 在未排序元素中继续寻找最小元素，并保存其下标
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j; //并保存其下标
+                }
+            }
+            // 将未排序列中最小元素放到已排序列末尾
+            swap(arr,minIndex,i);// 每一趟循环选出最小的值i的位置上，选完了数组有序
+        }
+    }
+    public static void swap(int[] arr,int a ,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b]=temp;
+    }
+    public static void printArray(int[] arr){
+        if(arr == null){
+            return;
+        }
+        for(int i = 0;i < arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+
+    //比数器的实现。
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+   public static int[] copyArray(int[] arr){
+       if(arr == null){
+           return null;
+       }
+       int[] tar  = new int[arr.length];
+       for(int i=0; i< arr.length;i++){
+           tar[i]  = arr[i];
+       }
+      return tar;
+   }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+//        int [] arr  = {3,42,1,5,7,27};
+//        selectionSort(arr);
+//        printArray(arr);
+
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            selectionSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
